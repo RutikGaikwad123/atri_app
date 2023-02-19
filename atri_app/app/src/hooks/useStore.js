a646af47daa154bc12ee5d3284c8133b371e4c28
rutik gaikwad <ruttikgaikwad03@gmail.com>
2023-02-19 21:49:21 +0530

    first commit

---
+++ atri_app/app/src/hooks/useStore.js
@@ -0,0 +1,324 @@
+import create from "zustand";
+
+// unsafe merge state
+// and mew properties will added or existing properties will be changed
+// but the type of value of the property must not change
+function mergeState(baseState, newState) {
+  if (
+    typeof newState === "object" &&
+    !Array.isArray(newState) &&
+    newState !== null
+  ) {
+    const keys = Object.keys(newState);
+    keys.forEach((key) => {
+      // create a new key in base if not exists
+      if (!(key in baseState)) {
+        baseState[key] = {};
+      }
+      if (typeof newState[key] === "object" && !Array.isArray(newState[key]))
+        mergeState(baseState[key], newState[key]);
+      else baseState[key] = newState[key];
+    });
+  }
+}
+
+const useStore = create((set) => {
+  return {
+    setPage: (pageName, newState) =>
+      set((state) => {
+        const pageState = JSON.parse(JSON.stringify(state[pageName]));
+        mergeState(pageState, newState);
+        return { [pageName]: pageState };
+      }),
+  };
+});
+
+export function updateStoreStateFromController(pageName, newState) {
+  useStore.getState().setPage(pageName, newState);
+}
+
+const desktopModeProps = {
+  ...{
+  "Home": {
+    "naveflex": {
+      "callbacks": {}
+    },
+    "navbar": {
+      "callbacks": {}
+    },
+    "comp": {
+      "callbacks": {}
+    },
+    "Flex4": {
+      "callbacks": {}
+    },
+    "Flex5": {
+      "callbacks": {}
+    },
+    "Flex6": {
+      "callbacks": {}
+    },
+    "Div1": {
+      "callbacks": {}
+    },
+    "Flex8": {
+      "callbacks": {}
+    },
+    "Flex9": {
+      "callbacks": {}
+    },
+    "Flex40": {
+      "callbacks": {}
+    },
+    "Flex11": {
+      "callbacks": {}
+    },
+    "Flex16": {
+      "callbacks": {}
+    },
+    "Flex17": {
+      "callbacks": {}
+    },
+    "Flex18": {
+      "callbacks": {}
+    },
+    "Image1": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/616da2c309aa0721dacedf0a_logo-restaurante-x-template.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox1": {
+      "custom": {
+        "text": "Home"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox2": {
+      "custom": {
+        "text": "About"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox3": {
+      "custom": {
+        "text": "Menu"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox4": {
+      "custom": {
+        "text": "Cart"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox5": {
+      "custom": {
+        "text": "Pages"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Button1": {
+      "custom": {
+        "text": "Order online"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Button2": {
+      "custom": {
+        "text": "Submit"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox6": {
+      "custom": {
+        "text": "eat burgers in LA."
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox8": {
+      "custom": {
+        "text": "The best place to"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox9": {
+      "custom": {
+        "text": "Lorem ipsum dolor sit amet consectetur adipiscing elit ugue quam diam vitae velit bibendum elementum eget non vivamus volutpat odio cras vestibulum purus aliquam."
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Button7": {
+      "custom": {
+        "text": "Reservate"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Button8": {
+      "custom": {
+        "text": "Order online"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image15": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/616d9a0b651769674022e7ba_home-hero-restaurante-x-template%20(1).jpeg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox10": {
+      "custom": {
+        "text": "Order from your favorite app today!"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image3": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/616d9a0bded3b95d9731ecc4_logo-postmates-restaurante-x-template.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image4": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/616d9a0b63b4ee3fe71f5072_logo-doorash-restaurante-x-template.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image5": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/2.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image6": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/616d9a0b5b82ce962fe1736b_logo-grubhub-restaurante-x-template.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    }
+  }
+}};
+
+useStore.setState(desktopModeProps);
+
+export default useStore;
---
+++ atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,181 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { Flex } from "@atrilabs/react-component-manifests/src/manifests/Flex/Flex.tsx";
+import { Div } from "@atrilabs/react-component-manifests/src/manifests/Div/Div.tsx";
+import { Image } from "@atrilabs/react-component-manifests/src/manifests/Image/Image.tsx";
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { Button } from "@atrilabs/react-component-manifests/src/manifests/Button/Button.tsx";
+import { usenaveflexCb, usenavbarCb, usecompCb, useFlex4Cb, useFlex5Cb, useFlex6Cb, useDiv1Cb, useFlex8Cb, useFlex9Cb, useFlex40Cb, useFlex11Cb, useFlex16Cb, useFlex17Cb, useFlex18Cb, useImage1Cb, useTextBox1Cb, useTextBox2Cb, useTextBox3Cb, useTextBox4Cb, useTextBox5Cb, useButton1Cb, useButton2Cb, useTextBox6Cb, useTextBox8Cb, useTextBox9Cb, useButton7Cb, useButton8Cb, useImage15Cb, useTextBox10Cb, useImage3Cb, useImage4Cb, useImage5Cb, useImage6Cb } from "../page-cbs/Home";
+import "../page-css/Home.css";
+import "../custom/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [location])
+
+  const naveflexProps = useStore((state)=>state["Home"]["naveflex"]);
+const naveflexIoProps = useIoStore((state)=>state["Home"]["naveflex"]);
+const naveflexCb = usenaveflexCb()
+const navbarProps = useStore((state)=>state["Home"]["navbar"]);
+const navbarIoProps = useIoStore((state)=>state["Home"]["navbar"]);
+const navbarCb = usenavbarCb()
+const compProps = useStore((state)=>state["Home"]["comp"]);
+const compIoProps = useIoStore((state)=>state["Home"]["comp"]);
+const compCb = usecompCb()
+const Flex4Props = useStore((state)=>state["Home"]["Flex4"]);
+const Flex4IoProps = useIoStore((state)=>state["Home"]["Flex4"]);
+const Flex4Cb = useFlex4Cb()
+const Flex5Props = useStore((state)=>state["Home"]["Flex5"]);
+const Flex5IoProps = useIoStore((state)=>state["Home"]["Flex5"]);
+const Flex5Cb = useFlex5Cb()
+const Flex6Props = useStore((state)=>state["Home"]["Flex6"]);
+const Flex6IoProps = useIoStore((state)=>state["Home"]["Flex6"]);
+const Flex6Cb = useFlex6Cb()
+const Div1Props = useStore((state)=>state["Home"]["Div1"]);
+const Div1IoProps = useIoStore((state)=>state["Home"]["Div1"]);
+const Div1Cb = useDiv1Cb()
+const Flex8Props = useStore((state)=>state["Home"]["Flex8"]);
+const Flex8IoProps = useIoStore((state)=>state["Home"]["Flex8"]);
+const Flex8Cb = useFlex8Cb()
+const Flex9Props = useStore((state)=>state["Home"]["Flex9"]);
+const Flex9IoProps = useIoStore((state)=>state["Home"]["Flex9"]);
+const Flex9Cb = useFlex9Cb()
+const Flex40Props = useStore((state)=>state["Home"]["Flex40"]);
+const Flex40IoProps = useIoStore((state)=>state["Home"]["Flex40"]);
+const Flex40Cb = useFlex40Cb()
+const Flex11Props = useStore((state)=>state["Home"]["Flex11"]);
+const Flex11IoProps = useIoStore((state)=>state["Home"]["Flex11"]);
+const Flex11Cb = useFlex11Cb()
+const Flex16Props = useStore((state)=>state["Home"]["Flex16"]);
+const Flex16IoProps = useIoStore((state)=>state["Home"]["Flex16"]);
+const Flex16Cb = useFlex16Cb()
+const Flex17Props = useStore((state)=>state["Home"]["Flex17"]);
+const Flex17IoProps = useIoStore((state)=>state["Home"]["Flex17"]);
+const Flex17Cb = useFlex17Cb()
+const Flex18Props = useStore((state)=>state["Home"]["Flex18"]);
+const Flex18IoProps = useIoStore((state)=>state["Home"]["Flex18"]);
+const Flex18Cb = useFlex18Cb()
+const Image1Props = useStore((state)=>state["Home"]["Image1"]);
+const Image1IoProps = useIoStore((state)=>state["Home"]["Image1"]);
+const Image1Cb = useImage1Cb()
+const TextBox1Props = useStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1IoProps = useIoStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1Cb = useTextBox1Cb()
+const TextBox2Props = useStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2IoProps = useIoStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2Cb = useTextBox2Cb()
+const TextBox3Props = useStore((state)=>state["Home"]["TextBox3"]);
+const TextBox3IoProps = useIoStore((state)=>state["Home"]["TextBox3"]);
+const TextBox3Cb = useTextBox3Cb()
+const TextBox4Props = useStore((state)=>state["Home"]["TextBox4"]);
+const TextBox4IoProps = useIoStore((state)=>state["Home"]["TextBox4"]);
+const TextBox4Cb = useTextBox4Cb()
+const TextBox5Props = useStore((state)=>state["Home"]["TextBox5"]);
+const TextBox5IoProps = useIoStore((state)=>state["Home"]["TextBox5"]);
+const TextBox5Cb = useTextBox5Cb()
+const Button1Props = useStore((state)=>state["Home"]["Button1"]);
+const Button1IoProps = useIoStore((state)=>state["Home"]["Button1"]);
+const Button1Cb = useButton1Cb()
+const Button2Props = useStore((state)=>state["Home"]["Button2"]);
+const Button2IoProps = useIoStore((state)=>state["Home"]["Button2"]);
+const Button2Cb = useButton2Cb()
+const TextBox6Props = useStore((state)=>state["Home"]["TextBox6"]);
+const TextBox6IoProps = useIoStore((state)=>state["Home"]["TextBox6"]);
+const TextBox6Cb = useTextBox6Cb()
+const TextBox8Props = useStore((state)=>state["Home"]["TextBox8"]);
+const TextBox8IoProps = useIoStore((state)=>state["Home"]["TextBox8"]);
+const TextBox8Cb = useTextBox8Cb()
+const TextBox9Props = useStore((state)=>state["Home"]["TextBox9"]);
+const TextBox9IoProps = useIoStore((state)=>state["Home"]["TextBox9"]);
+const TextBox9Cb = useTextBox9Cb()
+const Button7Props = useStore((state)=>state["Home"]["Button7"]);
+const Button7IoProps = useIoStore((state)=>state["Home"]["Button7"]);
+const Button7Cb = useButton7Cb()
+const Button8Props = useStore((state)=>state["Home"]["Button8"]);
+const Button8IoProps = useIoStore((state)=>state["Home"]["Button8"]);
+const Button8Cb = useButton8Cb()
+const Image15Props = useStore((state)=>state["Home"]["Image15"]);
+const Image15IoProps = useIoStore((state)=>state["Home"]["Image15"]);
+const Image15Cb = useImage15Cb()
+const TextBox10Props = useStore((state)=>state["Home"]["TextBox10"]);
+const TextBox10IoProps = useIoStore((state)=>state["Home"]["TextBox10"]);
+const TextBox10Cb = useTextBox10Cb()
+const Image3Props = useStore((state)=>state["Home"]["Image3"]);
+const Image3IoProps = useIoStore((state)=>state["Home"]["Image3"]);
+const Image3Cb = useImage3Cb()
+const Image4Props = useStore((state)=>state["Home"]["Image4"]);
+const Image4IoProps = useIoStore((state)=>state["Home"]["Image4"]);
+const Image4Cb = useImage4Cb()
+const Image5Props = useStore((state)=>state["Home"]["Image5"]);
+const Image5IoProps = useIoStore((state)=>state["Home"]["Image5"]);
+const Image5Cb = useImage5Cb()
+const Image6Props = useStore((state)=>state["Home"]["Image6"]);
+const Image6IoProps = useIoStore((state)=>state["Home"]["Image6"]);
+const Image6Cb = useImage6Cb()
+
+  return (<>
+  <Flex className="p-Home naveflex bpt" {...naveflexProps} {...naveflexCb} {...naveflexIoProps}>
+<Flex className="p-Home navbar bpt" {...navbarProps} {...navbarCb} {...navbarIoProps}>
+<Image className="p-Home Image1 bpt" {...Image1Props} {...Image1Cb} {...Image1IoProps}/>
+<Flex className="p-Home comp bpt" {...compProps} {...compCb} {...compIoProps}>
+<TextBox className="p-Home TextBox1 bpt" {...TextBox1Props} {...TextBox1Cb} {...TextBox1IoProps}/>
+<TextBox className="p-Home TextBox2 bpt" {...TextBox2Props} {...TextBox2Cb} {...TextBox2IoProps}/>
+<TextBox className="p-Home TextBox3 bpt" {...TextBox3Props} {...TextBox3Cb} {...TextBox3IoProps}/>
+<TextBox className="p-Home TextBox5 bpt" {...TextBox5Props} {...TextBox5Cb} {...TextBox5IoProps}/>
+<TextBox className="p-Home TextBox4 bpt" {...TextBox4Props} {...TextBox4Cb} {...TextBox4IoProps}/>
+</Flex>
+</Flex>
+<Flex className="p-Home Flex4 bpt" {...Flex4Props} {...Flex4Cb} {...Flex4IoProps}>
+<Button className="p-Home Button1 bpt" {...Button1Props} {...Button1Cb} {...Button1IoProps}/>
+<Button className="p-Home Button2 bpt" {...Button2Props} {...Button2Cb} {...Button2IoProps}/>
+</Flex>
+</Flex>
+<Flex className="p-Home Flex5 bpt" {...Flex5Props} {...Flex5Cb} {...Flex5IoProps}>
+<Flex className="p-Home Flex6 bpt" {...Flex6Props} {...Flex6Cb} {...Flex6IoProps}>
+<Div className="p-Home Div1 bpt" {...Div1Props} {...Div1Cb} {...Div1IoProps}>
+<TextBox className="p-Home TextBox8 bpt" {...TextBox8Props} {...TextBox8Cb} {...TextBox8IoProps}/>
+<TextBox className="p-Home TextBox6 bpt" {...TextBox6Props} {...TextBox6Cb} {...TextBox6IoProps}/>
+<TextBox className="p-Home TextBox9 bpt" {...TextBox9Props} {...TextBox9Cb} {...TextBox9IoProps}/>
+<Flex className="p-Home Flex8 bpt" {...Flex8Props} {...Flex8Cb} {...Flex8IoProps}>
+<Flex className="p-Home Flex9 bpt" {...Flex9Props} {...Flex9Cb} {...Flex9IoProps}>
+<Button className="p-Home Button7 bpt" {...Button7Props} {...Button7Cb} {...Button7IoProps}/>
+<Button className="p-Home Button8 bpt" {...Button8Props} {...Button8Cb} {...Button8IoProps}/>
+</Flex>
+</Flex>
+</Div>
+<Flex className="p-Home Flex40 bpt" {...Flex40Props} {...Flex40Cb} {...Flex40IoProps}>
+<Image className="p-Home Image15 bpt" {...Image15Props} {...Image15Cb} {...Image15IoProps}/>
+</Flex>
+</Flex>
+</Flex>
+<Flex className="p-Home Flex11 bpt" {...Flex11Props} {...Flex11Cb} {...Flex11IoProps}>
+<TextBox className="p-Home TextBox10 bpt" {...TextBox10Props} {...TextBox10Cb} {...TextBox10IoProps}/>
+<Flex className="p-Home Flex16 bpt" {...Flex16Props} {...Flex16Cb} {...Flex16IoProps}>
+<Flex className="p-Home Flex17 bpt" {...Flex17Props} {...Flex17Cb} {...Flex17IoProps}>
+<Image className="p-Home Image4 bpt" {...Image4Props} {...Image4Cb} {...Image4IoProps}/>
+<Image className="p-Home Image3 bpt" {...Image3Props} {...Image3Cb} {...Image3IoProps}/>
+</Flex>
+<Flex className="p-Home Flex18 bpt" {...Flex18Props} {...Flex18Cb} {...Flex18IoProps}>
+<Image className="p-Home Image5 bpt" {...Image5Props} {...Image5Cb} {...Image5IoProps}/>
+<Image className="p-Home Image6 bpt" {...Image6Props} {...Image6Cb} {...Image6IoProps}/>
+</Flex>
+</Flex>
+</Flex>
+  </>);
+}
